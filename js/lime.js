ed231759484e43ea9ae68778e30d5f4a6421dc63
Eduardo Santiago <stgocruzeduardo@gmail.com>
2021-09-01 15:59:28 -0500

    feat(html, js): added changes to project: slider effect in cards (Woking well) [rev-2]

--- js/lime.js
+++ js/lime.js
@@ -1,45 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  /*const navBar = document.querySelector('.navbar-burger');
-  if (navBar) {
-    navBar.addEventListener('click', () => {
-      // Get the target from the "data-target" attribute
-      const target =(navBar.dataset || {}).target;
-      const element = document.getElementById(target);
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      navBar.classList.toggle('is-active');
-      element.classList.toggle('is-active');
-    });
-  }*/
-  /*let navbar = document.querySelector('#lime-navbar');
-  console.log("Nav; ", navbar);
-  const offsetTopNav = navbar.offsetTop;
-  console.log("offfssettop ; ", offsetTopNav)*/
-  /*let body = document.getElementsByClassName('container-section-body');
-  console.log("VBodys ", body[0])
-  body[0].addEventListener('scroll', addedBehaviorNavFixed);
-  */
-  // new Splide('.splide').mount();
-
   addedSmoothEffect();
-
   function addedSmoothEffect() {
-    const links = [...document.querySelectorAll('.container-section-navbar nav ul li')];
+    const links = [...document.querySelectorAll('.container-section-navbar nav ul li a')];
     console.log("Links ::: ", links);
     links.forEach(link => link.addEventListener('click', smoothEffect));
   }
-
   function smoothEffect(e) {
-    console.log("E ", e)
-    e.prevetDefault();
+    e.preventDefault();
     const href = this.getAttribute('href');
-    console.log("Href: ", href)
-    const offsetTop = document.querySelector(href).offsetTop;
-    scroll({
-      top: offsetTop,
-      behavior: 'smooth'
+    document.querySelector(href).scrollIntoView({
+      behavior: "smooth"
     });
   }
-
   function addedBehaviorNavFixed() {
     // sconsole.log("Scroll ", document.querySelector('#limes-alert'), window.scrollY)
     /*const navbar = document.querySelector('#lime-navbar');
